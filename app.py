097e763eb1ee36978bd557a72b5a1a40f4a267ba
ppjotrek <pichor@student.agh.edu.pl>
2025-02-07 20:02:05 +0100

    obsluga templatek

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from werkzeug.utils import secure_filename
 import os
 from docx import Document
+from docxtpl import DocxTemplate
 
 
 app = Flask(__name__)
@@ -20,28 +21,23 @@ def index():
         combined_data['additional_data'] = session['additional_data']
     return render_template('index.html', combined_data=combined_data)
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'No file part'
-        file = request.files['file']
-        if file.filename == '':
-            return 'No selected file'
-        if file and (file.filename.endswith('.xlsx') or file.filename.endswith('.xls') or file.filename.endswith('.csv')):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
-                df = pd.read_excel(filepath)
-            elif file.filename.endswith('.csv'):
-                df = pd.read_csv(filepath)
-            session['data'] = df.to_dict()  # Zapisz dane do sesji
-            return redirect(url_for('index'))
-    return render_template('index.html')
+@app.route('/upload_and_submit', methods=['POST'])
+def upload_and_submit():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file and (file.filename.endswith('.xlsx') or file.filename.endswith('.xls') or file.filename.endswith('.csv')):
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        if file.filename.endswith('.xlsx') or file.filename.endswith('.xls'):
+            df = pd.read_excel(filepath)
+        elif file.filename.endswith('.csv'):
+            df = pd.read_csv(filepath)
+        session['data'] = df.to_dict()  # Zapisz dane do sesji
 
-@app.route('/submit', methods=['POST'])
-def submit_data():
     dropdown1 = request.form.get('project-dropdown')
     date_option = request.form.get('date_option')
     if date_option == 'single':
@@ -65,23 +61,16 @@ def generate_docx():
     if 'data' in session and 'additional_data' in session:
         data = session['data']
         additional_data = session['additional_data']
-        document = Document()
+        doc = DocxTemplate(os.path.join("docx_templates", "templatka.docx"))
+        #for count in data['Imię']:         #nie działa - nie pobiera nic, chyba musi być w returnie, więc albo jakoś to ogarnać subrutyną albo zip
         count = 0
         docs_name = additional_data['project-dropdown'] + data['Nazwisko'][str(count)] + '.docx' 
         docx_path = os.path.join(app.config['UPLOAD_FOLDER'], docs_name)
-        document.add_heading('Usprawiedliwienie', level=1)
-        document.add_heading('Dane', level=2)
-        for key, value in data.items():
-            document.add_heading(key, level=3)
-            for k, v in value.items():
-                document.add_paragraph(f'{k}: {v}')
-        document.add_heading('Dodatkowe informacje', level=2)
-        for key, value in additional_data.items():
-            document.add_paragraph(f'{key}: {value}')
-        document.save(docx_path)
+        context = { 'imie' : data['Imię'][str(count)], 'nazwisko' : data['Nazwisko'][str(count)], 'numer_indeksu' : data['Numer indeksu'][str(count)], 'data' : additional_data['date'] }
+        doc.render(context)
+        doc.save(docx_path)
         return send_file(docx_path, as_attachment=True)
     return 'No data available to generate document'
-
 
 if __name__ == '__main__':
     app.run(debug=True)
